21d9050e0361e4fb46024cdb0680c258b3cb19ae
Joshua Riek <jjriek@verizon.net>
2024-04-15 11:05:57 -0400

    fix

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ git clone https://github.com/canonical/ubuntu-image
 cd ubuntu-image || exit 1
 git checkout 7a3c433455dbe5f4c66fe53aa885c04b4af85a1d
 touch ubuntu-image.rst
+sed -i 's/               golang-go (>= 2:1.21~),/               golang-go (>= 2:1.17~),/g' debian/control
 sudo apt -y build-dep .
 dpkg-buildpackage -us -uc
 sudo dpkg -i ../*.deb
